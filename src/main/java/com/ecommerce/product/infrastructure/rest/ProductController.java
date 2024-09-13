796cf117a72338e95dc690824bc82eb8df09c52a
gbalcisoy <gbalcisoy@gmail.com>
2024-09-14 00:00:43 +0300

    commit test

--- src/main/java/com/ecommerce/product/infrastructure/rest/ProductController.java
+++ src/main/java/com/ecommerce/product/infrastructure/rest/ProductController.java
@@ -14,9 +14,7 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,13 +43,13 @@ public class ProductController {
         return new ProductRestResourceAssembler().toCollectionModel(products);
     }
 
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product createProduct(@RequestBody Product product) {
+        return productApplicationService.createProduct(product);
+    }
 //    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 //    public Product getProductById(@PathVariable String id) {
 //        return productApplicationService.getProductById(id);
 //    }
 //
-//    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    public Product addProduct(@RequestBody Product product) {
-//        return productApplicationService.addProduct(product);
-//    }
 }
